289fcbcf5d66f1e66648c59c7800af5c5852791a
Adam Morris <adam.morris@cern.ch>
2017-04-28 16:15:59 +0100

    correct test

--- pdfs/dalitz/test/src/SplineArgand.cc
+++ pdfs/dalitz/test/src/SplineArgand.cc
@@ -8,11 +8,11 @@ int main()
 {
 	std::vector<DPComplexSpline::complex_knot> knots =
 	{
-		{0.98, std::polar<double>(0.22,0.52)},
-		{1.00, std::polar<double>(0.15,0.50)},
-		{1.20, std::polar<double>(0.66,0.34)},
-		{1.40, std::polar<double>(0.43,0.30)},
-		{1.60, std::polar<double>(0.8,0.42)},
+		{0.98, std::polar<double>(0.1,0.00)},
+		{1.00, std::polar<double>(0.2,0.10)},
+		{1.20, std::polar<double>(0.5,0.34)},
+		{1.40, std::polar<double>(0.8,0.30)},
+		{1.60, std::polar<double>(1.0,0.42)},
 		{1.80, std::polar<double>(1.0,0.50)}
 	};
 	DPComplexSpline SplineShape(knots);
@@ -34,12 +34,12 @@ int main()
 	}
 	TCanvas can("canvas","",500,500);
 	// Argand diagram
-	TGraphPolar k_polargraph(k_mass.size(), k_radius.data(), k_theta.data());
+	TGraphPolar k_polargraph(k_mass.size(), k_theta.data(), k_radius.data());
 	k_polargraph.SetTitle("");
 	k_polargraph.SetMarkerStyle(8);
-	k_polargraph.SetMarkerSize(0.5);
+	k_polargraph.SetMarkerSize(1);
 	k_polargraph.Draw("AP");
-	TGraphPolar polargraph(mass.size(), radius.data(), theta.data());
+	TGraphPolar polargraph(mass.size(), theta.data(), radius.data());
 	polargraph.SetTitle("");
 	polargraph.SetLineColor(kRed);
 	polargraph.SetLineWidth(2);
@@ -49,7 +49,7 @@ int main()
 	TGraph k_absgraph(k_mass.size(), k_mass.data(), k_radius.data());
 	k_absgraph.SetTitle("");
 	k_absgraph.SetMarkerStyle(8);
-	k_absgraph.SetMarkerSize(0.5);
+	k_absgraph.SetMarkerSize(1);
 	k_absgraph.Draw("AP");
 	TGraph absgraph(mass.size(), mass.data(), radius.data());
 	absgraph.SetLineColor(kRed);
@@ -60,7 +60,7 @@ int main()
 	TGraph k_arggraph(k_mass.size(), k_mass.data(), k_theta.data());
 	k_arggraph.SetTitle("");
 	k_arggraph.SetMarkerStyle(8);
-	k_arggraph.SetMarkerSize(0.5);
+	k_arggraph.SetMarkerSize(1);
 	k_arggraph.Draw("AP");
 	TGraph arggraph(mass.size(), mass.data(), theta.data());
 	arggraph.SetLineColor(kRed);
