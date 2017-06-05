73e11172366b64dcdb941676aa554cfe649b2a89
Adam Morris <adam.morris@cern.ch>
2017-06-05 14:24:13 +0100

    quicker

--- pdfs/dalitz/src/DPBarrierFactor.cc
+++ pdfs/dalitz/src/DPBarrierFactor.cc
@@ -22,7 +22,7 @@ DPBarrierFactor::DPBarrierFactor(const unsigned _spin, const double _radius,  co
 			std::cerr << "Only up to spin-3 barrier factors are currently implemented. Defaulting to spin-0." << std::endl;
 			function = &DPBarrierFactor::FunctionL0;
 	}
-	precalcFF=function(radius*radius*p0*p0);
+	precalcFF = function(radius * radius * p0 * p0);
 }
 // Static functions for different spins
 double DPBarrierFactor::FunctionL0(const double z)
@@ -32,15 +32,15 @@ double DPBarrierFactor::FunctionL0(const double z)
 }
 double DPBarrierFactor::FunctionL1(const double z)
 {
-	return 1+z;
+	return 1 + z;
 }
 double DPBarrierFactor::FunctionL2(const double z)
 {
-	return z*z+3*z+9;
+	return z*z + 3*z + 9;
 }
 double DPBarrierFactor::FunctionL3(const double z)
 {
-	return z*z*z+6*z*z+45*z+225;
+	return z*z*z + 6*z*z + 45*z + 225;
 }
 // Evaluate the barrier factor
 double DPBarrierFactor::barrier(const double p) const
@@ -50,13 +50,13 @@ double DPBarrierFactor::barrier(const double p) const
 // Evaluate the square of the barrier factor
 double DPBarrierFactor::barrier_sq(const double p) const
 {
-	double z  = p*p*radius*radius;
+	double z  = p * p * radius * radius;
 	double factor = precalcFF/function(z);
 	return factor;
 }
 void DPBarrierFactor::setparameters(const double _radius, const double p0)
 {
-	radius=_radius;
-	precalcFF=function(radius*radius*p0*p0);
+	radius = _radius;
+	precalcFF = function(radius * radius * p0 * p0);
 }
 
